300b7613c19e4adaa09e3869c32733fc5c840e1b
dreamtle <deniil-bog@mail.ru>
2026-01-29 22:34:24 +0700

    Update __init__.py

--- games/__init__.py
+++ games/__init__.py
@@ -1,6 +1,5 @@
 from .fighter import FighterGameView
 from .tanks import TanksGameView
 from .dice import DicePokerView
-from .chess import ChessGameView
 from .minesweeper import MinesGameView
 from .snake import SnakeGameView
